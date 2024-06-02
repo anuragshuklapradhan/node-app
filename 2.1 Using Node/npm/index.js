60319052c36d142cb7aadcdf13b9f2bc79c94501
Anurag Shukla <88739561+anuragshuklapradhan@users.noreply.github.com>
2024-06-02 10:05:15 +0530

    Add files via upload

---
+++ 2.1 Using Node/npm/index.js
@@ -0,0 +1,13 @@
+// var generateName = require('sillyname');
+// import generateName from "sillyname";
+// // var sillyName = generateName();
+// console.log(`my name is  ${sillyName}`);
+// const superheroes = require('superheroes');
+import superheroes from 'superheroes';
+ 
+// superheroes.all;
+// //=> ['3-D Man', 'A-Bomb', …]
+ 
+ const name=superheroes.random();
+//=> 'Spider-Ham'
+console.log(`My name is ${name}`);;
---
+++ 2.1 Using Node/qr generator/index.js
@@ -0,0 +1,24 @@
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+  var qr = require('qr-image');
+ 
+var qr_svg = qr.image('I love QR!', { type: 'svg' });
+qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
+ 
+var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
+
+
